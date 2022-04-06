2b448a346c5238182ad93d2870ab88ba896e0836
YannMarti <100758384+YannMarti@users.noreply.github.com>
2022-04-06 11:52:00 +0200

    added Localization using easyLocalization

--- vidaia/lib/main.dart
+++ vidaia/lib/main.dart
@@ -1,7 +1,18 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
+  runApp(
+    EasyLocalization(
+        supportedLocales: [Locale('en', 'US'),],
+        path:
+            'assets/translations', // <-- change the path of the translation files
+        fallbackLocale: Locale('en', 'US'),
+        child: MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -11,6 +22,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      //Localization
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+      //
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
