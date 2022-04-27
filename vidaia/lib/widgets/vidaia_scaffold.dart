9635e6907012140dec48bb56f2210f7ad41f61bf
YannMarti <100758384+YannMarti@users.noreply.github.com>
2022-04-27 07:57:34 +0200

    added appbar shape

--- vidaia/lib/widgets/vidaia_scaffold.dart
+++ vidaia/lib/widgets/vidaia_scaffold.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:vidaia/widgets/vidaia_drawer.dart';
 import 'package:vidaia/widgets/vidaia_navbar.dart';
+import 'dart:math' as math;
+
+//padding to the right of vidar in appbar
+const double offsetRight = 25;
+//change inset to move circle farther into appbar
+const double inset = 20;
+//change r to change circle size(r is radius)
+const double r = 50;
 
 class VidaiScaffold extends StatelessWidget {
   final Widget body;
@@ -14,8 +22,47 @@ class VidaiScaffold extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       drawerEnableOpenDragGesture: false,
-      appBar: AppBar(
-        shape: CustomShapeBorder(),
+      appBar: PreferredSize(
+        preferredSize: const Size(double.infinity, 50),
+        child: Stack(children: [
+          AppBar(
+            shape: CustomShapeBorder(),
+
+            /*
+              Container(
+                //height: 80,
+                //width: 80,
+                margin: const EdgeInsets.all(20.0),
+                decoration: const BoxDecoration(
+                    color: Colors.orange, shape: BoxShape.circle),
+                child: Column(
+                  children: const [
+                    Expanded(child: Icon(Icons.monetization_on_outlined)),
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.all(8),
+                        child: Text("37,37 VID"),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            */
+          ),
+          Container(
+            width: double.infinity,
+            alignment: Alignment.bottomRight,
+            child: Padding(
+              padding: const EdgeInsets.only(right: offsetRight - 20 +(r - 80/2)/2),
+              child: Container(
+                height: 80,
+                width: 80,
+                decoration: const BoxDecoration(
+                    color: Colors.orange, shape: BoxShape.circle),
+              ),
+            ),
+          ),
+        ]),
       ),
       body: body,
       drawer: const VidaiaDrawer(),
@@ -24,6 +71,7 @@ class VidaiScaffold extends StatelessWidget {
   }
 }
 
+/*
 class CustomShapeBorder extends ContinuousRectangleBorder {
   @override
   Path getOuterPath(Rect rect, {TextDirection? textDirection}) {
@@ -43,6 +91,34 @@ class CustomShapeBorder extends ContinuousRectangleBorder {
     path.quadraticBezierTo(rect.width / 2 + (innerCircleRadius / 2) + 30,
         rect.height + 15, rect.width, rect.height);
     path.lineTo(rect.width, 0.0);
+    path.close();
+
+    return path;
+  }
+}
+*/
+
+class CustomShapeBorder extends ContinuousRectangleBorder {
+  @override
+  Path getOuterPath(Rect rect, {TextDirection? textDirection}) {
+    double oHeight = rect.height - inset;
+
+    //edit this to move the circle
+    final oval = Rect.fromCenter(
+        center: Offset(rect.width - (r/2) - offsetRight, oHeight),
+        width: r * 2,
+        height: r * 2);
+
+    Path path = Path();
+    path.lineTo(0, rect.height);
+    path.relativeLineTo(rect.width, 0);
+    path.relativeLineTo(0, -rect.height);
+
+    double h = oHeight - rect.height + r;
+    var alpha = 2 * math.acos(1 - (h / r));
+
+    path.addArc(oval, (math.pi - alpha) / 2, alpha);
+
     path.close();
 
     return path;
