4bf3f512ff9fe14b499a10d2f2799e488d8ffc3b
YannMarti <100758384+YannMarti@users.noreply.github.com>
2022-05-02 14:01:13 +0200

    added token transfer function

--- vidaia/lib/utils/globals.dart
+++ vidaia/lib/utils/globals.dart
@@ -1,6 +1,9 @@
 import 'dart:typed_data';
 
+import 'package:thor_request_dart/wallet.dart';
+
 bool loginStatus = false;
 
 List<String>? mnemonicPhrase;
 Uint8List? privateKey;
+Wallet? wallet;
--- vidaia/lib/utils/wallet.dart
+++ vidaia/lib/utils/wallet.dart
@@ -1,5 +1,7 @@
 import 'package:thor_devkit_dart/crypto/keystore.dart';
 import 'package:thor_devkit_dart/crypto/mnemonic.dart';
+import 'package:thor_request_dart/connect.dart';
+import 'package:thor_request_dart/wallet.dart';
 import 'package:vidaia/utils/globals.dart' as global;
 
 //TODO: change this to a secure save method for the words nd the key
@@ -7,19 +9,25 @@ createNewWallet() {
   assert(global.mnemonicPhrase == null);
   assert(global.privateKey == null);
 
+  
+
   //generate mnemonic phrase and save it on local device
   List<String> words = Mnemonic.generate();
   global.mnemonicPhrase = words;
 
   //derive privat key from words and save it on local device
   var priv = Mnemonic.derivePrivateKey(words);
+
+  //TODO: check if priv really needs to be saved
   global.privateKey = priv;
+  global.wallet = Wallet(priv);
 }
 
 //TODO: change this to a secure save method for the words nd the key
 recoverWalletFromWords(List<String> words) {
   assert(global.mnemonicPhrase == null);
   assert(global.privateKey == null);
+  assert(global.wallet == null);
 
   //save mnemonic phrase  on local device
   global.mnemonicPhrase = words;
@@ -27,4 +35,12 @@ recoverWalletFromWords(List<String> words) {
   //derive privat key from words and save it on local device
   var priv = Mnemonic.derivePrivateKey(words);
   global.privateKey = priv;
+}
+
+
+transferVidar(int value, String address, String url) {
+  assert(global.wallet != null);
+  Connect connect = Connect(url);
+  connect.transferToken(global.wallet!, address, 'tokenContractAddress', BigInt.from(value));
+
 }
