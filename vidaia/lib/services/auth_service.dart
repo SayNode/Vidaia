918561a7d06eecf3e636227316cbd9cd42f5cc27
Francesco Romeo <f.romeo.f@gmail.com>
2022-06-02 17:25:36 +0200

    wallet address is written in the Auth0 metadata on address creation

--- vidaia/lib/services/auth_service.dart
+++ vidaia/lib/services/auth_service.dart
@@ -10,8 +10,7 @@ import 'package:vidaia/models/auth0_user.dart';
 import 'package:pkce/pkce.dart';
 
 class AuthService {
-  Auth0User profile = Auth0User(
-      nickname: '', name: '', email: '', picture: '', updatedAt: '', sub: '');
+  Auth0User profile = Auth0User(nickname: '', name: '', email: '', picture: '', updatedAt: '', sub: '');
   Auth0IdToken? idToken;
   String? auth0AccessToken;
   bool isLoggedIn = false;
@@ -70,8 +69,7 @@ class AuthService {
       headers: {'Authorization': 'Bearer $auth0AccessToken'},
     );
 
-    debugPrint(
-        'logout: ${response.request} ${response.statusCode} ${response.body}');
+    debugPrint('logout: ${response.request} ${response.statusCode} ${response.body}');
 
     return 'logout: ${response.request} ${response.statusCode} ${response.body}';
   }
@@ -82,21 +80,13 @@ class AuthService {
         AUTH0_CLIENT_ID,
         AUTH0_REDIRECT_URI,
         issuer: AUTH0_ISSUER,
-        scopes: [
-          'openid',
-          'profile',
-          'email',
-          'offline_access',
-          'update:current_user_metadata',
-          'create:current_user_metadata'
-        ],
+        scopes: ['openid', 'profile', 'email', 'offline_access', 'update:current_user_metadata', 'create:current_user_metadata'],
         promptValues: ['login'],
 
         /// possible values login, none, consent, select_account
       );
 
-      final AuthorizationTokenResponse? result =
-          await appAuth.authorizeAndExchangeCode(
+      final AuthorizationTokenResponse? result = await appAuth.authorizeAndExchangeCode(
         authorizationTokenRequest,
       );
 
@@ -152,8 +142,7 @@ class AuthService {
   }
 
   Future<String> _setLocalVariables(result) async {
-    final bool isValidResult =
-        result != null && result.accessToken != null && result.idToken != null;
+    final bool isValidResult = result != null && result.accessToken != null && result.idToken != null;
 
     if (isValidResult) {
       auth0AccessToken = result.accessToken;
@@ -164,10 +153,8 @@ class AuthService {
           key: REFRESH_TOKEN_KEY,
           value: result.refreshToken,
         );
-        String? refreshTokenKeyFromStorage =
-            await secureStorage.read(key: REFRESH_TOKEN_KEY);
-        debugPrint('auth_service | REFRESH_TOKEN_KEY in storage is ' +
-            refreshTokenKeyFromStorage!);
+        String? refreshTokenKeyFromStorage = await secureStorage.read(key: REFRESH_TOKEN_KEY);
+        debugPrint('auth_service | REFRESH_TOKEN_KEY in storage is ' + refreshTokenKeyFromStorage!);
       }
       isLoggedIn = true;
       return 'Success';
@@ -179,50 +166,38 @@ class AuthService {
   Future<bool> updateUserWalletAddress(Auth0User auth0user) async {
     /// experiments to get the Management API token
     final managementApiTokenUrl = Uri.https(
-                                            AUTH0_DOMAIN,
-                                            '/oauth/token',
-                                          );
-    final managementAPIPostResponse = await http.post(
-        managementApiTokenUrl,
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: {
-          'client_id': 'HjuGFyrQbhsSfRby4dBR0GqTTGF9ysh7',
-          'client_secret': 'l1ofwHdiClPe5YK7ICKc6B97YPFCkCR7q68iLrPdk713rT8dTw4_-HXDbQEg-_72',
-          'grant_type': 'client_credentials',
-          'audience': 'https://flutter.vidaia.ch/api/v2/',
-        }
+      AUTH0_DOMAIN,
+      '/oauth/token',
     );
+    final managementAPIPostResponse = await http.post(managementApiTokenUrl, headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }, body: {
+      'client_id': 'HjuGFyrQbhsSfRby4dBR0GqTTGF9ysh7',
+      'client_secret': 'l1ofwHdiClPe5YK7ICKc6B97YPFCkCR7q68iLrPdk713rT8dTw4_-HXDbQEg-_72',
+      'grant_type': 'client_credentials',
+      'audience': 'https://dev-jp7b9rk6.us.auth0.com/api/v2/',
+    });
 
     var body = jsonDecode(managementAPIPostResponse.body);
     var accessToken = body['access_token'];
+    var scope = body['scope'];
     debugPrint('managementAPIPostResponse.body - ' + managementAPIPostResponse.body.toString());
+    debugPrint('managementAPIPostResponse.scope - ' + scope.toString());
 
     ///
 
-    final url = Uri.https(
-                            AUTH0_DOMAIN,
-                            '/api/v2/users/' + auth0user.id,
-                            {
-                              'audience': 'https://dev-jp7b9rk6.us.auth0.com/api/v2/'
-                            }
-                          );
+    final url = Uri.https(AUTH0_DOMAIN, '/api/v2/users/' + auth0user.id, {'audience': 'https://flutter.vidaia.ch/api/v2/'});
 
     debugPrint('update wallet request uri.https - ' + url.toString());
     debugPrint('update wallet auth0AccessToken -  $accessToken');
     debugPrint('update wallet request uri.https -  $idToken');
 
-    final responseAuth0UserInfo = await http.patch(
-        url,
-        headers: {
-          'Authorization': 'Bearer $accessToken',
-          'Content-Type': 'application/json'
-        },
+    final responseAuth0UserInfo = await http.patch(url,
+        headers: {'Authorization': 'Bearer $accessToken', 'Content-Type': 'application/json'},
         body: jsonEncode({
+          // "email": auth0user.email,
           "user_metadata": {"wallet_address": auth0user.walletAddress}
-        })
-    );
+        }));
     if (responseAuth0UserInfo.statusCode == 200) {
       debugPrint('update wallet response 200');
       return true;
